7baef0252b64e3670939c2429b995121bc87ff11
Aaditya Kalsi <akalsi.work@gmail.com>
2015-10-03 11:19:10 -0400

    Empty versions of timing routines

---
+++ include/perfcomp/time/cpu.h
@@ -0,0 +1,11 @@
+/**
+ * \file cpu.h
+ * \date 2015
+ */
+
+#ifndef PERFCOMP_TIME_CPU_H
+#define PERFCOMP_TIME_CPU_H
+
+
+
+#endif//PERFCOMP_TIME_CPU_H
---
+++ include/perfcomp/time/wall.h
@@ -0,0 +1,11 @@
+/**
+ * \file wall.h
+ * \date 2015
+ */
+
+#ifndef PERFCOMP_TIME_WALL_H
+#define PERFCOMP_TIME_WALL_H
+
+
+
+#endif//PERFCOMP_TIME_WALL_H
